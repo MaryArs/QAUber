830e3241818e3dd7f2c8bece8fdddf177f6660c3
MaryArs <mary.arsitova@gmail.com>
2018-06-04 13:46:17 -0700

    Documentation for class BankAccount using javadoc.

--- src/main/java/bank/BankAccount.java
+++ src/main/java/bank/BankAccount.java
@@ -1,11 +1,29 @@
 package bank;
-
+/**
+*This is a class which used to create a Bank Account Objects.
+* @author Marina Arsitova
+* */
 public class BankAccount {
+
+    /**
+    * accountNumber is an integer variable for storing  unique Account number;
+    * consumerName is a String variable for storing holder's name;
+    * balance is storing current balance of this account;
+    * isActive is storing a boolean status of account (is it active right now or not).
+    * */
+
     private int accountNumber;
     private String consumerName;
     private double balance;
     private boolean isActive;
 
+    /**
+    * This ia a constructor which creates Bank Account Object.
+    * @param accountNumber account number
+    * @param consumerName account holder name
+    * @param balance current balance of account
+    * @param isActive status of account
+    * */
     public BankAccount(int accountNumber, String consumerName, double balance, boolean isActive){
         this.accountNumber = accountNumber;
         this.consumerName = consumerName;
@@ -13,10 +31,18 @@ public class BankAccount {
         this.isActive = isActive;
     }
 
+    /**
+    *This method is used to deposit money from account.
+    * */
     public void deposit(double amount){
         balance = balance + amount;
     }
 
+    /**
+    * This method is used to withdraw money from account. In account should be minimum of 500$.
+    * @param amount How much do you want to withdraw.
+    * @throws MinimumBalanceException this will thrown if you are not keeping minimum of 500$ in account.
+    * */
     public void withdraw(double amount)throws MinimumBalanceException{
 
         if ((balance -amount) < 500){
@@ -29,6 +55,9 @@ public class BankAccount {
 
     }
 
+    /**
+     *This method is used to check balance in the account.
+     * */
     public double checkBalance(){
         return balance;
     }
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"bank","l":"BankAccount"},{"p":"bank","l":"MinimumBalanceException"}]
